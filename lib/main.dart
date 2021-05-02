37df9b904f2306c7d8662536159d9ad6ff3c223c
adrianocamamu <suportcamamu@gmail.com>
2021-05-01 21:09:35 -0300

    adicionado funcao floatingaction

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _TelaContadorState extends State<TelaContador> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meu Contador'),
+        title: Text('Contador'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -46,32 +46,38 @@ class _TelaContadorState extends State<TelaContador> {
             bottom: 10.0,
             height: 50.0,
             right: 10.0,
-            child: FloatingActionButton(
-              onPressed: () {
-                print('Botao Pressionado');
-                setState(() {
-                  resultado += 1;
-                });
-              },
-              child: Icon(Icons.add),
-            ),
+            child: OperationFloatingActionButton(Icon(Icons.add), () {
+              setState(() {
+                resultado += 1;
+              });
+            }),
           ),
           Positioned(
-            bottom: 10.0,
-            height: 50.0,
-            right: 70.0,
-            child: FloatingActionButton(
-              onPressed: () {
-                print('Botao Pressionado');
+              bottom: 10.0,
+              height: 50.0,
+              right: 70.0,
+              child: OperationFloatingActionButton(Icon(Icons.remove), () {
                 setState(() {
                   resultado -= 1;
                 });
-              },
-              child: Icon(Icons.remove),
-            ),
-          )
+              }))
         ],
       ),
+    );
+  }
+}
+
+class OperationFloatingActionButton extends StatelessWidget {
+  final Function func;
+  final Widget icon;
+
+  OperationFloatingActionButton(this.icon, this.func, {Key key})
+      : super(key: key);
+
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: func,
+      child: icon,
     );
   }
 }
